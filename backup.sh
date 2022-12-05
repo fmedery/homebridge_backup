b92aa28de22f16cdca298452c790a5d1096c891a
Frederic Medery <fmedery@gmail.com>
2022-12-04 19:30:27 -0500

    fix backup file name

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ set -e
 
 URI_HOST="http://homebridge.local:8581"
 BACKUP_FILE_PATH="$(pwd)/backups"
-BACKUP_FILE_NAME="backup-$(date '+%Y-%m-%d').tar.gz"
+BACKUP_FILE_NAME="backup-$(date '+%Y-%m-%d').tgz"
 
 ACCESS_TOKEN=$(curl --silent --location \
     --request POST ${URI_HOST}'/api/auth/login' \
@@ -15,4 +15,4 @@ ACCESS_TOKEN=$(curl --silent --location \
 curl --silent --location \
     --request GET "${URI_HOST}"'/api/backup/download' \
     -H "Authorization: bearer $ACCESS_TOKEN" \
-    -o ${BACKUP_FILE_PATH}/${BACKUP_FILE_NAME}.tgz
+    -o ${BACKUP_FILE_PATH}/${BACKUP_FILE_NAME}
