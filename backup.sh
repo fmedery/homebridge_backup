92469497a472d8f5273dcf0933dd5bbf5e6b666a
Frederic Medery <fmedery@gmail.com>
2022-12-04 19:10:21 -0500

    update

--- backup.sh
+++ backup.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-#
-# This script requires the "jq" package to be installed.
-# Linux: apt-get install jq
-# macOS: brew install jq
-#
-
-set -e -x
-
-
-if ! which jq
-then
-    echo  "jq is missing"
-    exit 1
-fi
+set -e
 
 URI_HOST="http://homebridge.local:8581"
 BACKUP_FILE_PATH="$(pwd)"
@@ -24,11 +11,9 @@ BACKUP_FILE_NAME="backup-$(date '+%Y-%m-%d').tar.gz"
 ACCESS_TOKEN=$(curl --silent --location \
     --request POST ${URI_HOST}'/api/auth/login' \
     --header 'Content-Type: application/json' \
-    --data-binary './access.json' | jq -r .access_token)
+    --data-binary '@./access.json' | jq -r .access_token)
 
-echo $ACCESS_TOKEN
-
-# # Download Backup
+# Download Backup
 curl --silent --location \
     --request GET "${URI_HOST}"'/api/backup/download' \
   -H "Authorization: bearer $ACCESS_TOKEN" \
